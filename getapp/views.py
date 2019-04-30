320bad46eb81b9b5008bbb52aabed435dbe42341
chastine1 <batamurizashastine@gmail.com>
2019-04-30 10:15:49 +0200

    I change

--- getapp/views.py
+++ getapp/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
-
+from .forms import NewProductForm
+from .models import Product
 # Create your views here.
 
 def welcome(request):
     return render(request, 'all-products/index.html')
 
+
+def product(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.save()
+        return redirect('welcome')
+
+    else:
+        form = NewProductForm()
+    return render(request, 'product.html', {"form": form})
 
 def search_results(request):
 
