5be3415dc266d6d1aeb5329deb02bdef9da3a4d8
chastine1 <batamurizashastine@gmail.com>
2019-05-02 09:06:39 +0200

    I change on models

--- getapp/admin.py
+++ getapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Store,Product,Supermarket,Location
+
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(Supermarket)
+admin.site.register(Location)
--- getapp/models.py
+++ getapp/models.py
@@ -1,15 +1,37 @@
 from django.db import models
 # from django.contrib.auth.models import User
 # Create your models here
-
-class Product(models.Model):
-    name = models.CharField(max_length=30)
-    price = models.IntegerField()
-    product_image = models.ImageField(upload_to = 'images/')
-    
+class Store(models.Model):
+    store_pic = models.ImageField(upload_to='stores/', blank=True, verbose_name="logo/picture")
+    store_name = models.CharField(max_length=30, verbose_name="store")
+    store_location = models.CharField(max_length=30, verbose_name="location name")
+    store_address = models.CharField(max_length=200, verbose_name="Address")
+    store_city = models.CharField(max_length=30, verbose_name="city")
+   
+    # delivery_area = models.ManyToManyField(Zipcode)
 
     def __str__(self):
-        return self.name
+        return str(self.store_name) + " (" + str(self.store_location) + ")"
+
+    class Meta:
+        ordering = ['store_name','store_city', 'store_location']
+    @classmethod
+    def get_store_by_id(cls, id):
+        store = cls.objects.get(pk=id)
+        return store 
+    @classmethod
+    def get_all_store(cls):
+        store= Store.objects.all()
+        return store 
+
+class Product(models.Model):
+    product_pic = models.ImageField(upload_to='images/', blank=True,null=True, verbose_name="product picture")
+    product_name = models.CharField(max_length=60, verbose_name="product",null=True)
+    product_price =models.IntegerField( default=0, verbose_name="price")
+    product_store = models.ManyToManyField(Store, verbose_name="availability(ies) in store(s)") 
+
+    class Meta:
+        ordering = ['product_name']
 
     def save_product(self):
         self.save()
@@ -18,7 +40,7 @@ class Product(models.Model):
         self.delete()
     @classmethod
     def search_by_name(cls,search_term):
-        product = cls.objects.filter(name__icontains=search_term)
+        product = cls.objects.filter(product_name__icontains=search_term)
         return product
     
 class Supermarket(models.Model):
